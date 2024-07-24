c913af8494fd6663fd37d0436087a3581d26a0fb
Adarsh katiyar <adarshkatiyar73@gmail.com>
2024-07-24 19:04:42 +0530

    Delete calculator.py

--- calculator.py
+++
@@ -1,9 +0,0 @@
-x=int(input())
-y=int(input())
-op=input()
-match op :
-    case "+": print(x,"+",y,"=",x+y)
-    case "-": print(x-y)
-    case "*": print(x*y)
-    case "/": print(x/y)
-    case _  : print("Not valid")
